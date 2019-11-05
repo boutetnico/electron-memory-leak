f055813ee730089b18f1f070b29404aa87b1ffc8
Nicolas Boutet <amd3002@gmail.com>
2019-11-05 17:47:21 +0100

    Improve memory usage logging

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function createThenDestroyWindow (delay) {
 
     createWindow(delay)
 
-    console.log(process.getHeapStatistics())
+    process.getProcessMemoryInfo().then(console.log)
 
   }, delay * 2)
 }
@@ -40,7 +40,7 @@ function createThenDestroyWindow (delay) {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-  createThenDestroyWindow(500)
+  createThenDestroyWindow(2000)
 })
 
 // Quit when all windows are closed.
